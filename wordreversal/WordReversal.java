737bd0287115a8ae74253b205285a691f210f655
KateSebring <ihatesuperman99@gmail.com>
2026-02-04 16:14:33 -0500

    Set up split function and added function for printing out split array for testing purposes

--- wordreversal/WordReversal.java
+++ wordreversal/WordReversal.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class WordReversal {
     // take in a string
     // divide up string into array by splitting at whitespace
@@ -8,7 +6,7 @@ public class WordReversal {
 
     private String original;
     private String reversed;
-    private ArrayList<String> split;
+    private String[] split;
     
     // initialize strings
     public WordReversal() {
@@ -24,7 +22,13 @@ public class WordReversal {
     // split original at each whitespace
     // and add to the split arraylist
     public void splitOriginal() {
+        split = this.original.split(" ");
+    }
 
+    public void printSplit() {
+        for(int i = 0; i < split.length; i++) {
+            System.out.println(split[i]);
+        }
     }
 
     // set reverse string
@@ -37,5 +41,12 @@ public class WordReversal {
     public void printValues() {
         System.out.println("Original input: " + this.original);
         System.out.println("Reversed input: " + this.reversed);
+    }
+
+    public static void main(String[] args) {
+        WordReversal reverseWord = new WordReversal();
+        reverseWord.setOriginal("dog and cat are friends");
+        reverseWord.splitOriginal();
+        reverseWord.printSplit();
     }
 }
