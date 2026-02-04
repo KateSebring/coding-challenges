8389a54615e3f7510ca5383c2d56167f14740159
KateSebring <ihatesuperman99@gmail.com>
2026-02-04 15:52:05 -0500

    planning and set up skeletons of functions and constructor

---
+++ wordreversal/WordReversal.java
@@ -0,0 +1,39 @@
+public class WordReversal {
+    // take in a string
+    // divide up string into array by splitting at whitespace
+    // create new string that reads the array backwards
+    // print out new string
+
+    private String original;
+    private String reversed;
+    private ArrayList split;
+    
+    // initialize strings
+    public void WordReversal() {
+        this.original = "";
+        this.reversed = "";
+    }
+
+    // set original based on input
+    public void setOriginal(String input) {
+        
+    }
+
+    // split original at each whitespace
+    // and add to the split arraylist
+    public void splitOriginal() {
+
+    }
+
+    // set reverse string
+    // by reading the split array backwards
+    public void setReverse() {
+
+    }
+
+    // print out strings
+    public void printValues() {
+        System.out.println("Original input: " + this.original);
+        System.out.println("Reversed input: " + this.reversed);
+    }
+}
